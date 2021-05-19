8cd08c10f9f943b02749a83cadc48704a69fe561
vanga997 <promtman1990@gmail.com>
2021-05-19 19:36:56 +0300

    а

--- MarriageAgency/Models/Nationality.cs
+++ MarriageAgency/Models/Nationality.cs
@@ -5,15 +5,11 @@ using System.ComponentModel.DataAnnotations;
 namespace MarriageAgency.Models {
 
     public class Nationality {
+
         public long NationalityID { get; set; }
 
         public string NationalityName { get; set; }
 
         public string Comments { get; set;}
-
-
     }
-
-
-
 }
